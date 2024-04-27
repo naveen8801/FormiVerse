1d376e06acb46872d5715aa26fb0abe1180c6e4a
naveen8801 <naveen.sharma@stackguardian.io>
2024-04-27 12:48:16 +0530

    Added  action

---
+++ src/actions/authActions.ts
@@ -0,0 +1,30 @@
+import User from "@/Models/User";
+import connectDB from "@/helpers/ConnectDB";
+import { hasPassword } from "@/helpers/passwordManager";
+
+export const handleUserSignUp = async (data: {
+  email: string;
+  password: string;
+  username: string;
+  fullname: string;
+}) => {
+  try {
+    const { username, email, password, fullname } = data;
+    const userCheck = await User.findOne({ email: email });
+    if (!userCheck) {
+      const hash = await hasPassword(password);
+      const newUser = await new User({
+        username: username,
+        email: email,
+        password: hash,
+        fullname: fullname,
+      }).save();
+
+      return { msg: "User created successfully" };
+    } else {
+      return { error: "User already exist with this email" };
+    }
+  } catch (error) {
+    return { error: error?.toString() };
+  }
+};
