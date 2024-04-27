6316e882e7c032ffef5784907cec658bebe01d98
naveen8801 <naveen.sharma@stackguardian.io>
2024-04-27 19:38:42 +0530

    Added DataTable component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,55 @@
 import { handleGetUserForms } from "@/actions/formActions";
 import { auth, config } from "@/auth";
+import { DataTable } from "@/components/DateTable";
+import { FORMS_TABLE_DEFINITION } from "@/constants";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+
+type Payment = {
+  id: string;
+  amount: number;
+  status: "pending" | "processing" | "success" | "failed";
+  email: string;
+};
+
+const payments: Payment[] = [
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "489e1d42",
+    amount: 125,
+    status: "processing",
+    email: "example@gmail.com",
+  },
+  {
+    id: "728ed522f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "728epp522f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "78ed5cd2f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "728d122f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+];
 
 export default async function Dashboard() {
   // Get session from server
@@ -14,5 +62,9 @@ export default async function Dashboard() {
 
   const { data, error } = await handleGetUserForms(session?.user?.id!);
 
-  return <>{}</>;
+  return (
+    <>
+      <DataTable columns={FORMS_TABLE_DEFINITION} data={payments} />
+    </>
+  );
 }
