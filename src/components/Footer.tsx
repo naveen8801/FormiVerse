44dbbf45750ff4084d8fcc85b4c730a004b52512
naveen8801 <naveen.sharma@stackguardian.io>
2024-07-26 12:12:42 +0530

    refactor

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,12 +5,12 @@ import { FaGithub } from "react-icons/fa";
 const Footer: React.FC = (): React.ReactElement => {
   return (
     <div className="w-full h-28 box-border flex gap-8 justify-end items-center px-16 ">
-      {/* <span className="text-sm text-slate-500 dark:text-slate-400">
+      <span className="text-sm text-slate-500 dark:text-slate-400">
         © Copyright 2024 FormiVerse. All rights reserved.
-      </span> */}
-      <Link href="https://github.com/naveen8801/FormiVerse" target="_blank">
+      </span>
+      {/* <Link href="https://github.com/naveen8801/FormiVerse" target="_blank">
         <FaGithub size={30} />
-      </Link>
+      </Link> */}
     </div>
   );
 };
