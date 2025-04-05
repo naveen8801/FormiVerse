832b90c5121128eb9e48a0b6c750409ed959434c
naveen8801 <naveen.sharma@stackguardian.io>
2025-04-05 12:07:50 +0530

    comment goole sign user create logic

--- src/auth.ts
+++ src/auth.ts
@@ -116,55 +116,55 @@ export const config = {
         token.fullname = user.fullname;
         token.isGoogleUser = false;
       }
-      if (account?.provider === "google") {
-        const currUser = await User.findOne({
-          email: user?.email,
-          isGoogleUser: true,
-        });
-        console.log("currUser inside jwt", currUser);
-        token.id = currUser._id;
-        token.username = currUser.username;
-        token.fullname = currUser.fullname;
-        token.isGoogleUser = true;
-      }
+      // if (account?.provider === "google") {
+      //   const currUser = await User.findOne({
+      //     email: user?.email,
+      //     isGoogleUser: true,
+      //   });
+      //   console.log("currUser inside jwt", currUser);
+      //   token.id = currUser._id;
+      //   token.username = currUser.username;
+      //   token.fullname = currUser.fullname;
+      //   token.isGoogleUser = true;
+      // }
       return token;
     },
     async signIn({ account, profile, user }: any) {
-      if (account?.provider === "google" && profile) {
-        try {
-          const { name, email, given_name } = profile;
-          const currUser = await User.findOne({ email, isGoogleUser: true });
-          console.log("currUser inside sign in", currUser);
-          if (!currUser) {
-            // Create a new user
-            const hash = await hasPassword(account?.providerAccountId);
-            const newUser = await new User({
-              username: generateUsername(name || given_name || ""),
-              email: email,
-              password: hash,
-              fullname: name || given_name || "",
-              isGoogleUser: true,
-            }).save({
-              validateBeforeSave: true,
-            });
-            user = newUser;
-            console.log("newUser inside sign in", newUser);
-            // Send email on user create
-            const { msg, error } = await sendEmail({
-              receiverEmail: email,
-              data: { name: name || given_name },
-              emailType: "WELCOME",
-            });
-            console.log("email sent for new user inside sign in");
-            return true;
-          }
-          user = currUser;
-          return true;
-        } catch (error) {
-          console.error("Error during google sign in", error);
-          return false;
-        }
-      }
+      // if (account?.provider === "google" && profile) {
+      //   try {
+      //     const { name, email, given_name } = profile;
+      //     const currUser = await User.findOne({ email, isGoogleUser: true });
+      //     console.log("currUser inside sign in", currUser);
+      //     if (!currUser) {
+      //       // Create a new user
+      //       const hash = await hasPassword(account?.providerAccountId);
+      //       const newUser = await new User({
+      //         username: generateUsername(name || given_name || ""),
+      //         email: email,
+      //         password: hash,
+      //         fullname: name || given_name || "",
+      //         isGoogleUser: true,
+      //       }).save({
+      //         validateBeforeSave: true,
+      //       });
+      //       user = newUser;
+      //       console.log("newUser inside sign in", newUser);
+      //       // Send email on user create
+      //       const { msg, error } = await sendEmail({
+      //         receiverEmail: email,
+      //         data: { name: name || given_name },
+      //         emailType: "WELCOME",
+      //       });
+      //       console.log("email sent for new user inside sign in");
+      //       return true;
+      //     }
+      //     user = currUser;
+      //     return true;
+      //   } catch (error) {
+      //     console.error("Error during google sign in", error);
+      //     return false;
+      //   }
+      // }
       return user;
     },
   },
